d74048105cf5c2a36da49939354a9185c138b3dc
Hajime Nakagami <nakagami@gmail.com>
2021-05-30 22:26:28 +0900

    decrate DB wrapper

--- pylevel/__init__.py
+++ pylevel/__init__.py
@@ -1,1 +1,19 @@
-from .rslevel import *
+from .rslevel import DB as BaseDB
+
+class DB:
+    def __init__(self, dirname, create_if_missing):
+        self.db = BaseDB(dirname, create_if_missing=create_if_missing)
+
+    def get(self, k):
+        if v:= self.db.get(k):
+            return bytes(v)
+        return None
+
+    def put(self, k, v):
+        self.db.put(k, v)
+
+    def delete(self, k):
+        self.db.delete(k)
+
+    def flush(self):
+        sef.flush()
