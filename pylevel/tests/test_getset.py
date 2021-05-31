4f35e8779f7a9f22769511d6d6ac646e73aa655a
Hajime Nakagami <nakagami@gmail.com>
2021-05-31 16:11:03 +0900

    add tests

--- pylevel/tests/test_getset.py
+++ pylevel/tests/test_getset.py
@@ -11,5 +11,16 @@ class TestGetSet(unittest.TestCase):
     def test_getset(self):
         self.assertEqual(self.db.get(b'key'), None)
 
+        self.db.put(b'key', b'value')
+        self.assertEqual(self.db.get(b'key'), b'value')
+
+        self.db.put(b'key', b'value2')
+        self.assertEqual(self.db.get(b'key'), b'value2')
+
+        self.db.delete(b'key')
+        self.assertEqual(self.db.get(b'key'), None)
+
+        self.db.flush()
+
     def tearDown(self):
         self.db.close()
